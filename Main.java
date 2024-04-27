866834a1ed8fd542ed3f2bbf7dcdf9b15bff919e
mirzayogy <mirza.yogy@gmail.com>
2024-04-27 10:23:10 +0800

    Scanner

--- Main.java
+++ Main.java
@@ -1,9 +1,24 @@
 import koneksi.Database;
+import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args){
-        int jumlahSks;
-        double ipk;
+        //tipeData variabel
+        int jumlahSks = 144;
+        double ipk = 4.0;
+        String nama = "Mirza";
         Database.hubungkan();
+
+
+        //Class object = new Constructor()
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Before: ");
+        System.out.println(nama);
+        System.out.print("Masukkan nama: ");
+        nama = scanner.nextLine();
+        System.out.println("After: ");
+        System.out.println(nama);
+
     }
 }
