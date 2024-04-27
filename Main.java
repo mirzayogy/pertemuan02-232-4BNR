ce356fc11883310054c179168579e7d3c5c455fe
mirzayogy <mirza.yogy@gmail.com>
2024-04-27 10:11:37 +0800

    menggunakan import

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
+import koneksi.Database;
+
 public class Main{
     public static void main(String[] args){
-        
+        int jumlahSks;
+        double ipk;
+        Database.hubungkan();
     }
 }
